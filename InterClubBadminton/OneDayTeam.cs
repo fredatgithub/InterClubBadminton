fd19ef69d7659599bbab29c37e4188af8ca92395
Freddy Juhel <domaine75@gmail.com>
2015-12-16 00:51:19 +0100

    Add properties to OneDayTeam

--- InterClubBadminton/OneDayTeam.cs
+++ InterClubBadminton/OneDayTeam.cs
@@ -25,6 +25,13 @@ namespace InterClubBadminton
   internal class OneDayTeam
   {
     public List<Player> Players { get; set; }
+    public Player SimpleMan1 { get; set; }
+    public Player SimpleMan2 { get; set; }
+    public Player SimpleMan3 { get; set; }
+    public Player SimpleWoman { get; set; }
+    public List<Player> SimpleDoubleMen { get; set; }
+    public List<Player> SimpleDoubleWomen { get; set; }
+    public List<Player> SimpleDoubleMixed { get; set; }
 
     public OneDayTeam()
     {
