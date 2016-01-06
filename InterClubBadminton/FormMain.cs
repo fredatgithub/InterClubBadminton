0c28e67a8844c98f259575b2f80c51b549391958
fred <domaine75@gmail.com>
2016-01-07 00:38:24 +0100

    Update FormMain.cs

--- InterClubBadminton/FormMain.cs
+++ InterClubBadminton/FormMain.cs
@@ -687,7 +687,7 @@ namespace InterClubBadminton
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
